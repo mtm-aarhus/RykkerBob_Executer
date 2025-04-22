899fa72c8f4093c70f5e5f1bf7c3248e18f92a80
gujc@aarhus.dk <nruXH246>
2025-04-22 13:00:39 +0200

    datastore

--- robot_framework/initialize.py
+++ robot_framework/initialize.py
@@ -2,12 +2,9 @@
 
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 
-
 def initialize(orchestrator_connection: OrchestratorConnection) -> None:
     """Do all custom startup initializations of the robot."""
     orchestrator_connection.log_trace("Initializing.")
     import Datastore
 
-    Datastore.out_ListOfProcessedItems.clear()
-    Datastore.out_ListOfErrorMessages.clear()
-    Datastore.out_ListOfFailedCases.clear()
+    Datastore.save_data(Datastore.default_data.copy())
